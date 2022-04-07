d55b3fd73c702a90a020f24b1df245ae325c9b0d
jeremias256 <jere.menacho@gmail.com>
2022-04-07 01:53:07 -0300

    c

--- curso_C/C_codeblocks_curso/165_punteros_2/main.c
+++ curso_C/C_codeblocks_curso/165_punteros_2/main.c
@@ -5,7 +5,7 @@ int main()
 {
 
     int numero = 50;
-    int *pNumero;
+    int *pNumero; //declaro un puntero tipo int
 
     printf("Dato: %d\n",numero); //imprime 50
     printf("Posicion: %p\n",&numero); //imprime pos memoria
@@ -14,14 +14,17 @@ int main()
 
     pNumero= &numero; //&numero es la posicion en memoria de numero
     printf("Valor de la variable:\n");
-    printf("Dato: %i\n",numero);
+    printf("Dato: %i\n",numero); //imprime num 50
     printf("Dato: %i\n",*pNumero);
+    //imprime lo que hay en la pos 000000000061FE14 QUE ES NUM=50
 
     printf("**************\n**************\n");
 
     printf("Posicion de memoria:\n");
     printf("Posicion: %p\n",&numero);
     printf("Posicion: %p\n",pNumero);
+
+
 
     return 0;
 }
